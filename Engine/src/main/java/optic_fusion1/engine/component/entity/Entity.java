ea9ebcef33b069b3294eb38e6dad8963db4e7a60
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2020-11-16 01:18:53 -0500

    Removed un-needed Entity code

--- Engine/src/main/java/optic_fusion1/engine/component/entity/Entity.java
+++ Engine/src/main/java/optic_fusion1/engine/component/entity/Entity.java
@@ -5,15 +5,8 @@ import optic_fusion1.engine.component.Component;
 
 public abstract class Entity extends Component {
 
-  private UUID uniqueId;
-  
   public Entity(String name) {
     super(name);
-    uniqueId = UUID.randomUUID();
-  }
-  
-  public UUID getUniqueId(){
-    return uniqueId;
   }
 
 }
